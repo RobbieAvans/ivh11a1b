f7c40c9cdff5fcb1740581f541df82159e89ed9f
Acra <sanderblijlevens@hotmail.com>
2016-03-02 17:03:15 +0100

    update 3

--- src/main/java/edu/avans/hartigehap/domain/agenda/AgendaItem.java
+++ src/main/java/edu/avans/hartigehap/domain/agenda/AgendaItem.java
@@ -3,21 +3,21 @@ package edu.avans.hartigehap.domain.agenda;
 import java.util.Date;
 
 public interface AgendaItem {
-	
+
 	/**
 	 * Get the startDate of the agendaItem
 	 * 
 	 * @return Date
 	 */
 	public Date getStartDate();
-	
+
 	/**
 	 * Get the endDate of the agendaItem
 	 * 
 	 * @return Date
 	 */
 	public Date getEndDate();
-	
+
 	/**
 	 * Get the description of the agendaItem
 	 * 
--- src/main/java/edu/avans/hartigehap/domain/agenda/Iterator.java
+++ src/main/java/edu/avans/hartigehap/domain/agenda/Iterator.java
@@ -1,14 +1,14 @@
 package edu.avans.hartigehap.domain.agenda;
 
 public interface Iterator {
-	
+
 	/**
 	 * Check if there is a next item
 	 * 
 	 * @return boolean
 	 */
 	public boolean hasNext();
-	
+
 	/**
 	 * Get the next item
 	 * 
