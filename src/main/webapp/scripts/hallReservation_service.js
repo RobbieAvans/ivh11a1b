3f501fb74aeca2d0dd2ccaa687b38f5debf3afad
tomgiesbergen <tomgiesbergen@live.nl>
2016-03-24 13:37:29 +0100

    Added CustomPartOfDay factory
    Because we had to define a custom PartOfDay object in Javacript

--- src/main/webapp/scripts/hallReservation_service.js
+++ src/main/webapp/scripts/hallReservation_service.js
@@ -7,4 +7,13 @@ return $resource('http://localhost:8082/hh/rest/v1/hallReservation/:id', { id: '
 		      method: 'DELETE'
 		    }
 	  });
+}).factory('CustomPartOfDay', function(){
+	function CustomPartOfDay(date,partOfDay){
+		this.date = date;
+		this.partOfDay = partOfDay;
+	};
+	CustomPartOfDay.build = function(data){
+		return new CustomPartOfDay(data.date, data.partOfDay);
+	};
+	return CustomPartOfDay;
 });
