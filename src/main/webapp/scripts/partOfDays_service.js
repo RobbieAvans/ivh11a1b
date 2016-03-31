87d005c29756110b0f3ae540ca1d3c4366574a7c
tomgiesbergen <tomgiesbergen@live.nl>
2016-03-31 15:31:33 +0200

    Added sessionid + minor change
    Added sessionid + minor change

--- src/main/webapp/scripts/partOfDays_service.js
+++ src/main/webapp/scripts/partOfDays_service.js
@@ -1,6 +1,7 @@
 angular.module('services.partofdays', []).factory('PartOfDays', function($resource) {
-	return $resource('http://localhost:8082/hh/rest/v1/partofday/:hallid/:weeknmr', { 
+	return $resource('http://localhost:8082/hh/rest/v1/partofday/:hallid/:weeknmr/:sessionid', { 
 		hallid: '@hallid',
-		weeknmr: '@weeknmr'
+		weeknmr: '@weeknmr',
+		sessionid : '@sessionid'
 	});
 });
