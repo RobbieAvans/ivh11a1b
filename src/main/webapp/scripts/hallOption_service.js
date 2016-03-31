a0cc6e116cb68a52bee7bd101f2d509b7a6f8b7d
tomgiesbergen <tomgiesbergen@live.nl>
2016-03-31 15:31:16 +0200

    Added sessionid by rest request
    Added sessionid by rest request

--- src/main/webapp/scripts/hallOption_service.js
+++ src/main/webapp/scripts/hallOption_service.js
@@ -1,5 +1,5 @@
 angular.module('services.hallOption', []).factory('HallOption', function($resource) {
-return $resource('http://localhost:8082/hh/rest/v1/halloption/:id', { id: '@_id' }, {
+return $resource('http://localhost:8082/hh/rest/v1/halloption/:id/:sessionid', { id: '@_id',sessionid : '@sessionid' }, {
 	    update: {
 	      method: 'PUT'
 	    },
--- src/main/webapp/scripts/hallReservation_service.js
+++ src/main/webapp/scripts/hallReservation_service.js
@@ -1,5 +1,5 @@
 angular.module('services.hallReservation', []).factory('HallReservation', function($resource) {
-return $resource('http://localhost:8082/hh/rest/v1/hallReservation/:id', { id: '@_id' }, {
+return $resource('http://localhost:8082/hh/rest/v1/hallReservation/:id/:sessionid', { id: '@_id',sessionid : '@sessionid' }, {
 	    update: {
 	      method: 'PUT'
 	    },
